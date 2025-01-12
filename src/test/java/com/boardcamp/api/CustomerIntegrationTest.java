20a65968ffac8b16f1b7e45e2cca708196a029fc
Ayrton Tales Silva do Nascimento <ayrtontsn@hotmail.com>
2025-01-12 19:24:36 -0300

    Tests: UnhappyPath create customers, get customers happy and unhappyPath

--- src/test/java/com/boardcamp/api/CustomerIntegrationTest.java
+++ src/test/java/com/boardcamp/api/CustomerIntegrationTest.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.boardcamp.api.dtos.CustomersDTO;
+import com.boardcamp.api.models.CustomersModel;
 import com.boardcamp.api.repositories.CustomersRepository;
 import com.boardcamp.api.repositories.GamesRepository;
 import com.boardcamp.api.repositories.RentalsRepository;
@@ -45,7 +46,7 @@ public class CustomerIntegrationTest {
     @Test
     void givenCustomerRepositoryEmpty_whenCreatingCustomer_thenCreateCustomer(){
         //given
-        CustomersDTO customer = new CustomersDTO("Test","12345678910","12345678910");
+        CustomersDTO customer = new CustomersDTO("Test01","12345678910","12345678910");
 
 
         HttpEntity<CustomersDTO> body = new HttpEntity<>(customer);
@@ -58,6 +59,72 @@ public class CustomerIntegrationTest {
         //then
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
         assertEquals(1, customersRepository.count());
+    }
+
+    @Test
+    void givenRepeatedCustomer_whenCreatingCustomer_thenThrowsError(){
+        //given
+        CustomersDTO customer = new CustomersDTO("Test02","12345678910","12345678910");
+        CustomersModel  customerRepeated = new CustomersModel(customer);
+        
+        customersRepository.save(customerRepeated);
+
+        HttpEntity<CustomersDTO> body = new HttpEntity<>(customer);
+        
+        //when
+        ResponseEntity<String> response = restTemplate.exchange(
+            "/customers",
+            HttpMethod.POST,
+            body,
+            String.class);
+
+        // then
+        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+        assertEquals(1, customersRepository.count());
+    }
+
+    @Test
+    void givenCreatedCustomer_whenGetCustomerById_thenReturnCustomer(){
+        //given
+        CustomersDTO customer = new CustomersDTO("Test03","12345678910","12345678910");
+        CustomersModel  customerCreater = new CustomersModel(customer);
+        
+        CustomersModel customerCreated = customersRepository.save(customerCreater);
+        
+        //when
+        ResponseEntity<CustomersDTO> response = restTemplate.exchange(
+            "/customers/{id}",
+            HttpMethod.GET,
+            null,
+            CustomersDTO.class,
+            customerCreated.getId());
+
+        // then
+        CustomersDTO returnedCustomer = response.getBody();
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(customerCreated.getName(), returnedCustomer.getName());
+    }
+
+    @Test
+    void givenCreatedAndDeletedCustomer_whenGetCustomerById_thenThrowErrors(){
+        //given
+        CustomersDTO customer = new CustomersDTO("Test03","12345678910","12345678910");
+        CustomersModel  customerCreater = new CustomersModel(customer);
+        
+        CustomersModel customerCreated = customersRepository.save(customerCreater);
+        customersRepository.deleteById(customerCreated.getId());
+        
+        //when
+        ResponseEntity<CustomersDTO> response = restTemplate.exchange(
+            "/customers/{id}",
+            HttpMethod.GET,
+            null,
+            CustomersDTO.class,
+            customerCreated.getId());
+
+        // then
+        
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
 }
